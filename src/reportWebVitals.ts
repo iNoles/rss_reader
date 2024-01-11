68e7eb6f71de45f5c64833ab43acf70c10bb4188
Jonathan Steele <iNoles@users.noreply.github.com>
2024-01-10 22:54:27 -0500

    more warnings!

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,6 +1,6 @@
-import {onCLS, onFID, onLCP, onTTFB} from 'web-vitals';
+import { onCLS, onFID, onLCP, onTTFB } from 'web-vitals'
 
-onCLS(console.log);
-onFID(console.log);
-onLCP(console.log);
-onTTFB(console.log);
+onCLS(console.log)
+onFID(console.log)
+onLCP(console.log)
+onTTFB(console.log)
