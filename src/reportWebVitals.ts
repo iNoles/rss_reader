5b2d4d54bec52b6b9b02b1fd7b7f3329383b4a76
Jonathan <iNoles@users.noreply.github.com>
2024-01-10 22:44:03 -0500

    Update reportWebVitals.ts

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,15 +1,4 @@
-import { type ReportCallback } from 'web-vitals'
-
-const reportWebVitals = () => (onPerfEntry: ReportCallback) => {
-  if ((onPerfEntry != null) && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry)
-      getFID(onPerfEntry)
-      getFCP(onPerfEntry)
-      getLCP(onPerfEntry)
-      getTTFB(onPerfEntry)
-    })
-  }
-}
-
-export default reportWebVitals
+import {onCLS, onFID, onLCP} from 'web-vitals';
+onCLS(console.log);
+onFID(console.log);
+onLCP(console.log);
