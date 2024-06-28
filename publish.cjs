ffc838dc1e249fbb67fedf0b50c3075f9d4a2390
Jonathan Steele <iNoles@users.noreply.github.com>
2024-06-28 00:38:37 -0400

    Delete publish.cjs

--- publish.cjs
+++
@@ -1,8 +0,0 @@
-var ghpages = require('gh-pages')
-var path = require('path')
-
-ghpages.publish('dist'),
-  function (err) {
-    console.log('Error publishing to gh-pages branch...')
-    console.log(err)
-  }
