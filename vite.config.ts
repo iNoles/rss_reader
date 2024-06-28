515ed39cd7692499316e255d6f1584ee76d390a0
Jonathan Steele <iNoles@users.noreply.github.com>
2024-06-28 00:44:03 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/rss-reader/',
+  base: '/rss_reader/',
   plugins: [vue()],
   resolve: {
     alias: {
